7c1b30508953bf291dac9aa85f898b6b5a876780
Aliyan91 <aliyanamir184@gmail.com>
2025-08-01 11:44:06 +0500

    done

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
@@ -68,8 +69,8 @@ export const createUserOrder = inngest.createFunction(
     },
   },
   { event: "order/created" },
-  async ({ event }) => {
-    const orders = EventSource.map((event) => {
+  async ({ events }) => {
+    const orders = events.map((event) => {
       return {
         userId: event.data.userId,
         items: event.data.items,
@@ -79,7 +80,7 @@ export const createUserOrder = inngest.createFunction(
       };
     });
     await connectDB();
-    await OrderPlaced.insertMany(orders);
+    await Order.insertMany(orders);
 
     return { success: true, message: "Order Created" };
   }
